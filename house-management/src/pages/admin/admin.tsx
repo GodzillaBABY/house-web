fcdc08f86668616f985086568d9054f1a99cba74
tuhaixia@heytea.com <199798c>
2020-11-10 18:45:45 +0800

    first commit

--- house-management/src/pages/admin/admin.tsx
+++ house-management/src/pages/admin/admin.tsx
@@ -4,7 +4,7 @@ import styles from './admin.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page login</h1>
+      <h1 className={styles.title}>Page loginloginlogin</h1>
     </div>
   );
 }
