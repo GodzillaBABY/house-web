9ac58955b038914fb990cb22df676763e42bbd6a
sunry <tansongyue@heytea.com>
2020-11-10 17:38:34 +0800

    测试1

--- house-management/src/pages/admin/admin.tsx
+++ house-management/src/pages/admin/admin.tsx
@@ -4,7 +4,7 @@ import styles from './admin.less';
 export default () => {
   return (
     <div>
-      <h1 className={styles.title}>Page login</h1>
+      <h1 className={styles.title}>Page login1111</h1>
     </div>
   );
 }
